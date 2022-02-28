c721961979e21134f1d7a9efd69d34c5bfc6408a
Rakibul Hasan Redoy <59376085+rakibulhasan247@users.noreply.github.com>
2022-02-28 12:46:58 +0600

    finished closure

--- A Closer Look at Functions/app.js
+++ A Closer Look at Functions/app.js
@@ -189,7 +189,7 @@ const addTax = (rate, value) => value - value * rate;
 console.log(addTax(0.2,525)); 
 
 */
-
+/* 
 const bdfly = {
     airline: 'Fly BD',
     iataCode:'BDFLY',
@@ -256,3 +256,69 @@ runOnce();
 }());
 
 (() => console.log('This will also never run again'))();
+
+
+ */
+
+/* 
+
+const secureBooking = function(){
+    let passengerCount =0;
+
+    return function(){
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+*/ 
+
+let f;
+
+
+const g = function() {
+    const a =23;
+    f = function(){
+        console.log(a*2);
+    };
+};
+
+
+const h = function() {
+    const b =50;
+    f = function(){
+        console.log(b*2);
+    };
+};
+
+
+g();
+f();
+console.dir(f);
+
+h();
+f();
+console.dir(f);
+
+//Example 2
+
+const boardPassengers = function(n,wait){
+    const perGroup = n/3;
+
+    setTimeout(function(){
+        console.log(`We are now boarding all ${n} passengers`);
+
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+
+};
+
+const perGroup = 1000;
+boardPassengers(180,3);
